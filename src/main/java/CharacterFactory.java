c006b2c3e07441a20cce4619de742831dd848e66
Sofiia Folvarochna <sofiyafolv@gmail.com>
2022-10-13 21:55:16 +0300

    small changes

--- src/main/java/CharacterFactory.java
+++ src/main/java/CharacterFactory.java
@@ -1,24 +1,28 @@
+import lotr.Character;
 import lotr.Noble;
 import org.reflections.Reflections;
-
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 import java.util.Random;
 import java.util.Set;
 
 import static org.reflections.scanners.Scanners.SubTypes;
 
 public class CharacterFactory {
-    Class createCharacter(){
-        Reflections reflections = new Reflections("com.my.project");
+    Character createCharacter() throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+        Reflections reflections = new Reflections("lotr");
         Set<Class<?>> subTypes =
                 reflections.get(SubTypes.of(Character.class).asClass());
         subTypes.remove(Noble.class);
         Class[] all_characters = new Class[subTypes.size()];
         int i = 0;
-        for(Class x: subTypes) {
-            all_characters[i] = x;
-            i++;
+        for (Class x:
+             subTypes) {
+            all_characters[i++] = x;
         }
         int rnd = new Random().nextInt(all_characters.length);
-        return all_characters[rnd];
+        Constructor constr = all_characters[rnd].getConstructor();
+        Character c = (Character) constr.newInstance();
+        return c;
     }
 }
