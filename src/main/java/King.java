a38b987701307125d8d182f13725e8ce216a535b
Sofiia Folvarochna <sofiyafolv@gmail.com>
2022-10-13 19:39:20 +0300

    some changes

---
+++ src/main/java/King.java
@@ -0,0 +1,7 @@
+import java.util.Random;
+
+public class King extends Noble{
+    public King(){
+        super(15, 5);
+    }
+}
---
+++ src/main/java/Knight.java
@@ -0,0 +1,8 @@
+import java.util.Random;
+
+public class Knight extends Noble{
+    public Knight(){
+        super(12, 2);
+    }
+
+}
